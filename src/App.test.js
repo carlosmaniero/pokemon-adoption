bd49c2688f500dc6db575bfc3ac5f58809e96798
Carlos Maniero <carlosmaniero@gmail.com>
2018-06-21 00:21:46 -0300

    App test now needs a provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={configureStore()}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
