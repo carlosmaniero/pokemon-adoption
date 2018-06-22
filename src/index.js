3885f7da1a225e35ac977c17e6673d7804deaaae
Carlos Maniero <carlosmaniero@gmail.com>
2018-06-21 22:38:41 -0300

    Removes the preloaded list of pokemons
    Specie list can fetch the list from the api. So it is not needed
    to have pre configure the application.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import { actions } from './species/actions';
 import { Provider } from 'react-redux';
 
 const store = configureStore();
-store.dispatch(actions.addPokemons(['bulbasaur', 'pikachu']));
-
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
